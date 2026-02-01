9dafbd438eb45a8c4a620dc799f1d5dda1dc4063
xasya89 <xasya89@mail.ru>
2026-02-01 13:42:00 +0300

    Изменена история документов

--- OnlineCashRmk/FormHistory.cs
+++ OnlineCashRmk/FormHistory.cs
@@ -10,25 +10,29 @@ using System.Windows.Forms;
 using Microsoft.EntityFrameworkCore;
 using OnlineCashRmk.Models;
 using OnlineCashRmk.DataModels;
+using OnlineCashTransportModels.Shared;
 
 namespace OnlineCashRmk
 {
     public partial class FormHistory : Form
     {
-        DataContext _db;
-        List<DocSynch> docSynches = new List<DocSynch>();
+        private readonly IDbContextFactory<DataContext> _dbFactory;
         public FormHistory(IDbContextFactory<DataContext> dbFactory)
         {
-            _db = dbFactory.CreateDbContext();
+            _dbFactory = dbFactory;
             InitializeComponent();
-            FormClosed += (s, e) => { _db.Dispose(); };
-
-            docSynches = _db.DocSynches
-                .Where(d => DateTime.Compare(d.Create.Date, DateTime.Now.AddDays(-10).Date) > 0 || DateTime.Compare(d.Create.Date, DateTime.Now.Date) == 0)
-                .OrderBy(d=>d.Create).ToList();
-            listBox1.DataSource = docSynches;
-
             
+            GetHistory();
+        }
+
+        async void GetHistory()
+        {
+            using var db = _dbFactory.CreateDbContext();
+            var docSynches = await db.DocSynches
+                .Where(d => DateTime.Compare(d.Create.Date, DateTime.Now.AddDays(-5).Date) > 0 || DateTime.Compare(d.Create.Date, DateTime.Now.Date) == 0)
+                .OrderByDescending(d => d.Create)
+                .AsNoTracking().ToListAsync();
+            listBox1.DataSource = docSynches;
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -46,28 +50,44 @@ namespace OnlineCashRmk
                 RenderArrival(docSynch.DocId);
             if (docSynch.TypeDoc == TypeDocs.CashMoney)
                 RenderCashMoney(docSynch.DocId);
-            if (docSynch.TypeDoc == TypeDocs.NewGoodFromCash)
-                RenderNewGood(docSynch.DocId);
             if (docSynch.TypeDoc == TypeDocs.Revaluation)
                 RenderRevaluation(docSynch.DocId);
         }
 
-        private void RenderOpenShift(int docId)
+        private async void RenderOpenShift(int docId)
         {
-            Shift shift = _db.Shifts.Where(s => s.Id == docId).FirstOrDefault();
+            using var _db = _dbFactory.CreateDbContext();
+            Shift shift = await _db.Shifts.Where(s => s.Id == docId)
+                .AsNoTracking().FirstAsync();
             richTextBox1.Text = $"Открытие смены {shift.Start.ToString("dd.MM.yy HH:mm")}"+Environment.NewLine;
         }
 
-        private void RenderCloseShift(int docId)
+        private async void RenderCloseShift(int docId)
         {
-            Shift shift = _db.Shifts.Where(s => s.Id == docId).FirstOrDefault();
-            richTextBox1.Text = $"Закрытие смены {shift.Start.ToString("dd.MM.yy HH:mm")}\n\n Итоги смены:\n ---------------- \nНаличные:\t{shift.SumNoElectron} \nБезналичные:\t{shift.SumElectron}  \nПродажи за сегодня:\t{shift.SumSell}\n ---------------- \nВозвраты:\t{shift.SummReturn}";
+            using var _db = _dbFactory.CreateDbContext();
+            Shift shift = await _db.Shifts.Where(s => s.Id == docId)
+                .AsNoTracking().FirstAsync();
+            richTextBox1.Text = @$"Закрытие смены {shift.Start.ToString("dd.MM.yy HH:mm")}
+
+Итоги смены:
+---------------- 
+Наличные:   {shift.SumNoElectron} 
+Безналичные:    {shift.SumElectron}  
+Продажи за сегодня:\t{shift.SumSell}
+---------------- 
+Возвраты:   {shift.SummReturn}
+---------------- 
+Акция 2+1: {shift.PromotionSum}";
         }
 
-        private void RenderArrival(int docId)
+        private async void RenderArrival(int docId)
         {
-            Arrival arrival = _db.Arrivals.Where(a => a.Id == docId)
-                .Include(a => a.ArrivalGoods).ThenInclude(a => a.Good).Include(a=>a.Supplier).FirstOrDefault();
+            using var _db = _dbFactory.CreateDbContext();
+            Arrival arrival = await _db.Arrivals.Where(a => a.Id == docId)
+                .Include(a => a.ArrivalGoods)
+                .ThenInclude(a => a.Good)
+                .Include(a => a.Supplier)
+                .AsNoTracking().FirstAsync();
             List<ArrivalPositionDataModel> positions = new List<ArrivalPositionDataModel>();
             var goods = _db.Goods.ToList();
             foreach (var arrivalGood in arrival.ArrivalGoods)
@@ -143,9 +163,14 @@ namespace OnlineCashRmk
             richTextBox1.Rtf = richTextBox1.Rtf.Replace("@@@", table.ToString());
         }
 
-        private void RenderWriteOf(int docId)
+        private async void RenderWriteOf(int docId)
         {
-            var writeof = _db.Writeofs.Include(w => w.WriteofGoods).ThenInclude(g => g.Good).Where(w => w.Id == docId).FirstOrDefault();
+            using var _db = _dbFactory.CreateDbContext();
+            var writeof = await _db.Writeofs
+                .Include(w => w.WriteofGoods)
+                .ThenInclude(g => g.Good)
+                .Where(w => w.Id == docId)
+                .AsNoTracking().FirstAsync();
             richTextBox1.Text = $@"Списание товара от {writeof.DateCreate.ToString("dd.MM.yy")}
 На сумму - {writeof.SumAll.ToSellFormat()}
 {writeof.Note}
@@ -180,26 +205,31 @@ namespace OnlineCashRmk
             richTextBox1.Rtf = richTextBox1.Rtf.Replace("@@@", table.ToString());
         }
 
-        private void RenderBuy(int docId)
+        private async void RenderBuy(int docId)
         {
-            /*
-            var check = _db.CheckSells.Include(c=>c.CheckPayments).Include(c=>c.CheckGoods).ThenInclude(c=>c.Good)
-                .Where(c=>c.Id==docId).FirstOrDefault();
-            string document = $@"Покупка {check.DateCreate.ToString("dd.MM.yy HH:mm")} на сумму {check.CheckPayments.Sum(p=>p.Sum).ToSellFormat()}
+            using var _db = _dbFactory.CreateDbContext();
+            var check = await _db.CheckSells
+                .Include(c => c.CheckPayments)
+                .Include(c => c.CheckGoods)
+                .ThenInclude(c => c.Good)
+                .Where(c => c.Id == docId)
+                .AsNoTracking().FirstAsync();
+            string document = $@"{(check.TypeSell==TypeSell.Sell ? "Покупка" : "Возврат")} {check.DateCreate.ToString("dd.MM.yy HH:mm")} на сумму {check.CheckPayments.Sum(p=>p.Sum).ToSellFormat()}
 Оплаты
 @payments
 
 Чек
 @@@";
             string paymentsStr = "";
-            foreach (var payment in check.CheckPayments)
-                paymentsStr += $"{payment.TypePaymentStr}  {payment.Sum}\n";
+            paymentsStr += check.SumElectron > 0 ? " Безнал: " + check.SumElectron : "";
+            paymentsStr += check.SumCash > 0 ? " Нал: " + check.SumCash : "";
             document = document.Replace("@payments", paymentsStr);
             richTextBox1.Text = document;
 
-            string[] cells = new string[4] { "Наименование", "Цена", "Кол-во", "Стоимость" };
+            string[] cells = new string[5] { "Наименование", "Цена", "Кол-во", "Акция", "Сумма" };
+            var checkGoods = check.CheckGoods.ToList();
             RtfTable tableCheckGoods = new RtfTable(check.CheckGoods.Count() + 1, cells.Length, 120);
-            tableCheckGoods.SetColumnWidths(1500, 720, 950, 1100);
+            tableCheckGoods.SetColumnWidths(2500, 720, 950, 950, 1100);
             for (int r = 0; r < check.CheckGoods.Count() + 1; r++)
                 for (int c = 0; c < cells.Length; c++)
                     if (r == 0)
@@ -208,49 +238,51 @@ namespace OnlineCashRmk
                         switch (c)
                         {
                             case 0:
-                                tableCheckGoods.Contents[r, c] = check.CheckGoods[r-1].Good?.Name;
+                                tableCheckGoods.Contents[r, c] = checkGoods[r-1].Good?.Name;
                                 break;
                             case 1:
-                                tableCheckGoods.Contents[r, c] = check.CheckGoods[r-1].Cost.ToSellFormat();
+                                tableCheckGoods.Contents[r, c] = checkGoods[r-1].Cost.ToSellFormat();
                                 break;
                             case 2:
-                                tableCheckGoods.Contents[r, c] = check.CheckGoods[r - 1].Count.ToString();
+                                tableCheckGoods.Contents[r, c] = checkGoods[r - 1].Count.ToString();
                                 break;
                             case 3:
-                                tableCheckGoods.Contents[r, c] = check.CheckGoods[r - 1].Sum.ToSellFormat();
+                                var promotionQuantity = checkGoods[r - 1].PromotionQuantity == 0 ? 
+                                    "" : checkGoods[r - 1].PromotionQuantity.ToString();
+                                tableCheckGoods.Contents[r, c] = promotionQuantity;
+                                break;
+                            case 4:
+                                tableCheckGoods.Contents[r, c] = checkGoods[r - 1].Sum.ToSellFormat();
                                 break;
                         };
             richTextBox1.Rtf=richTextBox1.Rtf.Replace("@@@", tableCheckGoods.ToString());
-            */
+            
         }
 
-        private void RenderCashMoney(int docId)
+        private async void RenderCashMoney(int docId)
         {
-            CashMoney cashMoney = _db.CashMoneys.Where(m => m.Id == docId).FirstOrDefault();
+            using var _db = _dbFactory.CreateDbContext();
+            CashMoney cashMoney = await _db.CashMoneys.Where(m => m.Id == docId)
+                .AsNoTracking().FirstAsync();
             richTextBox1.Text = @$"{cashMoney.TypeOperation.GetDisplay()} от {cashMoney.Create.ToString("dd.MM.yy")}
 На сумму {cashMoney.Sum.ToSellFormat()}
 {cashMoney.Note}";
         }
 
-        private void RenderNewGood(int docId)
+        private async void RenderRevaluation(int docId)
         {
-            NewGoodFromCash newGood = _db.NewGoodsFromCash.Include(g => g.Good).Where(g => g.Id == docId).FirstOrDefault();
-            richTextBox1.Text = @$"Товар - {newGood.Good.Name}
-Ед - {newGood.Good.Unit.GetDisplay()}
-Тип - {newGood.Good.SpecialType.GetDisplay()}
-Цена - {newGood.Good.Price.ToSellFormat()}";
-        }
-
-        private void RenderRevaluation(int docId)
-        {
-            /*
-            var revaluation = _db.Revaluations.Include(r=>r.RevaluationGoods).ThenInclude(r=>r.Good).Where(r => r.Id == docId).FirstOrDefault();
+            using var _db = _dbFactory.CreateDbContext();
+            var revaluation = await _db.Revaluations
+                .Include(r => r.RevaluationGoods).ThenInclude(r => r.Good)
+                .Where(r => r.Id == docId)
+                .AsNoTracking().FirstAsync();
             richTextBox1.Text = $@"Переоценка товара от {revaluation.Create.ToString("dd.MM.yy")}
 
 @@@";
             string[] cells = new string[7] { "Наименование", "Ед", "Кол-во", "Цена", "Сумма", "Цена", "Сумма" };
             RtfTable table = new RtfTable(revaluation.RevaluationGoods.Count + 1, cells.Length, 120);
             table.SetColumnWidths(800, 720, 750, 750, 750, 750, 750);
+            var revaluationGoods = revaluation.RevaluationGoods.ToList();
             for (int r = 0; r < revaluation.RevaluationGoods.Count + 1; r++)
                 for (int c = 0; c < cells.Length; c++)
                     if (r == 0)
@@ -259,29 +291,28 @@ namespace OnlineCashRmk
                         switch (c)
                         {
                             case 0:
-                                table.Contents[r, c] = revaluation.RevaluationGoods[r - 1].Good.Name;
+                                table.Contents[r, c] = revaluationGoods[r - 1].Good.Name;
                                 break;
                             case 1:
-                                table.Contents[r, c] = revaluation.RevaluationGoods[r - 1].Good.Unit.GetDisplay();
+                                table.Contents[r, c] = revaluationGoods[r - 1].Good.Unit.GetDisplay();
                                 break;
                             case 2:
-                                table.Contents[r, c] = revaluation.RevaluationGoods[r - 1].Count?.ToString();
+                                table.Contents[r, c] = revaluationGoods[r - 1].Count?.ToString();
                                 break;
                             case 3:
-                                table.Contents[r, c] = revaluation.RevaluationGoods[r - 1].PriceOld.ToSellFormat();
+                                table.Contents[r, c] = revaluationGoods[r - 1].PriceOld.ToSellFormat();
                                 break;
                             case 4:
-                                table.Contents[r, c] = revaluation.RevaluationGoods[r - 1].SumOld.ToSellFormat();
+                                table.Contents[r, c] = revaluationGoods[r - 1].SumOld.ToSellFormat();
                                 break;
                             case 5:
-                                table.Contents[r, c] = revaluation.RevaluationGoods[r - 1].PriceNew.ToSellFormat();
+                                table.Contents[r, c] = revaluationGoods[r - 1].PriceNew.ToSellFormat();
                                 break;
                             case 6:
-                                table.Contents[r, c] = revaluation.RevaluationGoods[r - 1].SumNew.ToSellFormat();
+                                table.Contents[r, c] = revaluationGoods[r - 1].SumNew.ToSellFormat();
                                 break;
                         };
             richTextBox1.Rtf = richTextBox1.Rtf.Replace("@@@", table.ToString());
-            */
         }
     }
 
